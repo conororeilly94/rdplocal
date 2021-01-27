2f7e487124eefb80d2747f918058a6519e01eab3
Conor O Reilly <x17101701@student.ncirl.ie>
2021-01-27 22:42:52 +0000

    added change password

--- rdpproject/main/views.py
+++ rdpproject/main/views.py
@@ -6,6 +6,7 @@ from category.models import Category
 from subcategory.models import SubCategory
 from django.contrib.auth import authenticate, login, logout
 from django.core.files.storage import FileSystemStorage
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -170,3 +171,65 @@ def contact(request):
     popularposts2 = Posts.objects.all().order_by('-views')[:3]
 
     return render(request, 'front/contact.html', {'site':site, 'posts':posts, 'category':category, 'subcategory':subcategory, 'lastposts':lastposts, 'popularposts2':popularposts2})
+
+
+def change_pass(request):
+
+    # Authenticating user
+    if not request.user.is_authenticated:
+        return redirect('mylogin')
+    # End login check
+
+    if request.method == 'POST':
+
+        # Get input
+        oldpass = request.POST.get('oldpass')
+        newpass = request.POST.get('newpass')
+
+        if oldpass == "" or newpass == "":
+
+            error = "All Fields Required"
+            return render(request, 'back/error.html', {'error':error})
+
+        user = authenticate(username = request.user, password = oldpass)
+
+        if user != None:
+
+            if len(newpass) < 8:
+
+                error = "Your Password Must be at least 8 Characters"
+                return render(request, 'back/error.html', {'error':error})
+
+            # Check strenth of new password
+            count1 = 0
+            count2 = 0
+            count3 = 0
+            count4 = 0
+
+            for i in newpass :
+    
+                if i > "0" and i < "9" :
+                    count1 = 1
+                if i > "A" and i < "Z" :
+                    count2 = 1
+                if i > 'a' and i < 'z' :
+                    count3 = 1
+                if i > "!" and i < "@" :
+                    count4 = 1
+
+            if count1 == 1 and count2 == 1 and count3 == 1 and count4 == 1 :
+
+                user = User.objects.get(username=request.user)
+                user.set_password(newpass)
+                user.save()
+                return redirect('mylogout')
+
+            print(count1, count2, count3, count4)
+
+        else:
+
+            error = "Your Password is Incorrect"
+            return render(request, 'back/error.html', {'error':error})
+
+
+    return render(request, 'back/changepass.html')
